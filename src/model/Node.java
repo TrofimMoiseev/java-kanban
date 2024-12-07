a3a0808be1414ffbe991ed0d6c416a9a20815881
Trofim Moiseev <moiseev.trofim11@yandex.com>
2024-12-08 00:26:21 +0500

    Sprint 6, tracker. java-kanban: добавить функциональность в хистори менеджер.

--- src/model/Node.java
+++ src/model/Node.java
@@ -1,11 +1,11 @@
 package model;
 
-public class Node <Task> {
+public class Node<T extends Task> {
     public Task task;
-    public Node<Task> next;
-    public Node<Task> prev;
+    public Node<T> next;
+    public Node<T> prev;
 
-    public Node(Node<Task> prev, Task task, Node<Task> next) {
+    public Node(Node<T> prev, Task task, Node<T> next) {
         this.prev = prev;
         this.task = task;
         this.next = next;
